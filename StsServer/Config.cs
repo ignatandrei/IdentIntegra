b4bc285998a4e36f35d1469897e124c1fa9ec7de
ignatandrei <ignatandrei@yahoo.com>
2019-12-18 07:57:36 +0200

    start

---
+++ StsServer/Config.cs
@@ -0,0 +1,83 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace StsServer
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new IdentityResource[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+        }
+
+        public static IEnumerable<ApiResource> GetApis()
+        {
+            return new ApiResource[]
+            {
+                new ApiResource("scope_used_for_hybrid_flow", "Mvc Hybrid Client"),
+                new ApiResource("native_api", "Native Client API")
+                {
+                    ApiSecrets =
+                    {
+                        new Secret("native_api_secret".Sha256())
+                    }
+                    //Scopes =
+                    //{
+                    //    new Scope
+                    //    {
+                    //        Name = "native_api"
+                    //    }
+                    //}
+                }
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new[]
+            {
+                // MVC client using hybrid flow
+                new Client
+                {
+                    ClientId = "hybridclient",
+                    ClientName = "MVC Client",
+
+                    AllowedGrantTypes = GrantTypes.HybridAndClientCredentials,
+                    ClientSecrets = { new Secret("hybrid_flow_secret".Sha256()) },
+
+                    RedirectUris = { "https://localhost:44381/signin-oidc" },
+                    FrontChannelLogoutUri = "https://localhost:44381/signout-oidc",
+                    PostLogoutRedirectUris = { "https://localhost:44381/signout-callback-oidc" },
+
+                    AllowOfflineAccess = true,
+                    AllowedScopes = { "openid", "profile", "offline_access",  "scope_used_for_hybrid_flow" }
+                },
+                new Client
+                {
+                    ClientId = "native.code",
+                    ClientName = "Native Client (Code with PKCE)",
+
+                    RedirectUris = { "https://127.0.0.1:45656" },
+                    PostLogoutRedirectUris = { "https://127.0.0.1:45656" },
+
+                    RequireClientSecret = false,
+
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RequirePkce = true,
+                    AllowedScopes = { "openid", "profile", "email", "native_api" },
+
+                    AllowOfflineAccess = true,
+                    RefreshTokenUsage = TokenUsage.ReUse
+                 }
+            };
+        }
+    }
+}
---
+++ StsServer/Startup.cs
@@ -0,0 +1,115 @@
+﻿using StsServer.Data;
+using StsServer.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Server.IISIntegration;
+using IdentityServer4;
+using Microsoft.IdentityModel.Tokens;
+
+namespace StsServer
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            services
+                .AddAuthentication(IISDefaults.AuthenticationScheme)
+                .AddOpenIdConnect("aad", "Sign-in with Azure AD", options =>
+                {
+                    options.Authority = "https://login.microsoftonline.com/common";
+                    options.ClientId = "https://leastprivilegelabs.onmicrosoft.com/38196330-e766-4051-ad10-14596c7e97d3";
+
+                    options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+                    options.SignOutScheme = IdentityServerConstants.SignoutScheme;
+
+                    options.ResponseType = "id_token";
+                    options.CallbackPath = "/signin-aad";
+                    options.SignedOutCallbackPath = "/signout-callback-aad";
+                    options.RemoteSignOutPath = "/signout-aad";
+
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = false,
+                        ValidAudience = "165b99fd-195f-4d93-a111-3e679246e6a9",
+
+                        NameClaimType = "name",
+                        RoleClaimType = "role"
+                    };
+                })
+
+                ;
+            //services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
+            //        .AddNegotiate(); 
+            services.Configure<IISOptions>(iis =>
+            {
+                iis.AuthenticationDisplayName = "Windows";
+                iis.AutomaticAuthentication = true;
+            });
+
+            var builder = services.AddIdentityServer(options =>
+            {
+                options.Events.RaiseErrorEvents = true;
+                options.Events.RaiseInformationEvents = true;
+                options.Events.RaiseFailureEvents = true;
+                options.Events.RaiseSuccessEvents = true;
+            })
+                .AddInMemoryIdentityResources(Config.GetIdentityResources())
+                .AddInMemoryApiResources(Config.GetApis())
+                .AddInMemoryClients(Config.GetClients())
+                .AddAspNetIdentity<ApplicationUser>();
+
+            builder.AddDeveloperSigningCredential();
+
+            services.AddControllersWithViews()
+                 .SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseIdentityServer();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+        }
+    }
+}
