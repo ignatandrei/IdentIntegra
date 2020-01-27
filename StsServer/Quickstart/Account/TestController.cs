e207c194b5ca7f695d5f1f3447b10e77f8524dba
Andrei Ignat <ignatandrei@yahoo.com>
2020-01-27 23:38:45 +0200

    authorize blind

--- StsServer/Quickstart/Account/TestController.cs
+++ StsServer/Quickstart/Account/TestController.cs
@@ -11,6 +11,9 @@ using IdentityServer4.Services;
 using IdentityServer4.Stores;
 using Microsoft.AspNetCore.Authentication;
 using IdentityServer4.Events;
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+using IdentityModel;
 
 namespace StsServer.Quickstart.Account
 {
@@ -38,7 +41,48 @@ namespace StsServer.Quickstart.Account
             _schemeProvider = schemeProvider;
             _events = events;
         }
+        public async Task<bool> AuthorizeBlind()
+        {
+            //TODO: 
+            //in browser call https://localhost:44364/test/authorizeblind
+            //then call https://localhost:44364/
+            #region initialize user, claims
+            string provider = "Windows";
+            string providerUserId = "Andrei";//does not matter
+            var localSignInProps = new AuthenticationProperties();//see what you need
 
+            //copied after AutoProvisionUserAsync
+            var user = new ApplicationUser
+            {
+                UserName = Guid.NewGuid().ToString(),
+            };
+            var filtered = new List<Claim>(){
+                new Claim(JwtClaimTypes.Name, "Radu Ignat"),
+                new Claim(JwtClaimTypes.Email, "asd@asd.com")
+            };
+            //TODO: add any other claims
+            #endregion
+
+            var identityResult = await _userManager.CreateAsync(user);
+            if (!identityResult.Succeeded) throw new Exception(identityResult.Errors.First().Description);
+
+            identityResult = await _userManager.AddClaimsAsync(user, filtered);
+            if (!identityResult.Succeeded) throw new Exception(identityResult.Errors.First().Description);
+
+            identityResult = await _userManager.AddLoginAsync(user, new UserLoginInfo(provider, providerUserId, provider));
+            if (!identityResult.Succeeded) throw new Exception(identityResult.Errors.First().Description);
+
+            // issue authentication cookie for user
+            // we must issue the cookie maually, and can't use the SignInManager because
+            // it doesn't expose an API to issue additional claims from the login workflow
+            var principal = await _signInManager.CreateUserPrincipalAsync(user);
+
+            var name = principal.FindFirst(JwtClaimTypes.Name)?.Value ?? user.Id;
+            await _events.RaiseAsync(new UserLoginSuccessEvent(provider, providerUserId, user.Id, name));
+            await HttpContext.SignInAsync(user.Id, name, provider, localSignInProps, filtered.ToArray());
+            
+            return true;
+        }
         public async Task<bool> AuthorizeDB(LoginInputModel model)
         {
             if (!ModelState.IsValid)
